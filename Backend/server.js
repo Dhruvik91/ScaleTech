c66f51af65a8ea56c17227f9f5ef1c1fb201e7be
Divyanshu Motivaras <divyanshu1593@gmail.com>
2023-08-22 17:32:07 +0530

    making server_2.js as main server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
-"use strict";
-
+const express = require("express");
 const pg = require('pg');
-const http = require('http');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const cors = require('cors');
 
-// connecting to ElephantSQL
-let conString = "postgres://otalcrol:JcHKJRmTiPo0eKntu6R5zi3zq0L6PuBu@drona.db.elephantsql.com/otalcrol" //Can be found in the Details page
-let client = new pg.Client(conString);
 
+// setting environment variables
+require('dotenv').config();
+
+// connecting to database
+let client = new pg.Client(process.env.DB_URL);
 
 client.connect((err) => {
     if(err) {
@@ -16,217 +18,167 @@ client.connect((err) => {
 });
 
 
+const app = express();
 
-// creating server
-const server = http.createServer((request, response) => {
-    // setting headers for CORS
-    let headers = request.rawHeaders;
-        
-    response.writeHead(200, {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Method': headers[headers.indexOf('Access-Control-Request-Method') + 1],
-        'Access-Control-Allow-Headers': headers[headers.indexOf('Access-Control-Request-Headers') + 1],
-    });
-    if (request.method == 'OPTIONS'){
-        response.end();
-    }
+// middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-    // APIs
 
-    // making api for storing data to database
-    else if (request.url == '/signUp') {
+// making API for storing signup data to the database
 
-        // API key to be returned in response
+app.get('/signUp', (req, res) => {
+    let signUp = {};
+
+    signUp.isSuccess = false;
+    signUp.errorMessage = `method can't be 'GET', it has to be 'POST'`;
+
+    res.send(JSON.stringify(signUp));
+});
+
+
+app.post('/signUp', (req, res) => {
+    if (('firstName' in req.body) && 
+    ('lastName' in req.body) && 
+    ('email' in req.body) && 
+    ('password' in req.body) && 
+    ('userName' in req.body)){
+
+        addSignUpInfo(req.body.firstName, req.body.lastName, req.body.email, req.body.password, req.body.userName, res);
+    } else {
+        // if some of the fields are missing in api request
         let signUp = {};
 
-        if (request.method != 'POST') {
+        signUp.isSuccess = false;
+        signUp.errorMessage = "Some of the fields were missing in post request";
 
-            signUp.isSuccess = false;
-            signUp.errorMessage = "The Method has to be POST";
-
-            response.write( JSON.stringify(signUp) );
-            response.end();
-        } else {
-            let body = '';
-
-            // for handling error
-            request.on('error', (err) => {
-                if(err) {
-                    response.writeHead(500, {'Content-Type': 'text/html'});
-                    response.write('An error occurred');
-                    response.end();
-                }
-            });
-
-            // reading chunks of POST data
-            request.on('data', chunk => {
-                body += chunk.toString();
-            });
-
-            request.on('end', () => {
-                // use parse() method
-                body = JSON.parse(body);
-    
-                if (('firstName' in body) && ('lastName' in body) && ('email' in body) && ('password' in body) && ('userName' in body)){
-
-                    addSignUpInfo(body.firstName, body.lastName, body.email, body.password, body.userName, signUp, response);
-                } else {
-                    // if some of the fields are missing in api request
-                    signUp.isSuccess = false;
-                    signUp.errorMessage = "Some of the fields were missing in post request";
-
-                    response.write( JSON.stringify(signUp) );
-                    response.end();
-                }
-            });
-        }
-
-    }
-    
-    // API for signin
-    else if (request.url == '/signIn'){
-        // API key to be returned in response
-        let signIn = {};
-
-        if (request.method != 'POST'){
-            signIn.isSuccess = false;
-            signIn.errorMessage = "Method must be POST";
-
-            response.write( JSON.stringify(signIn) );
-            response.end()
-        } else {
-            let body = '';
-
-            // for handling error
-            request.on('error', (err) => {
-                if(err) {
-                    response.writeHead(500, {'Content-Type': 'text/html'});
-                    response.write('An error occurred');
-                    response.end();
-                }
-            });
-
-            // reading chunks of POST data
-            request.on('data', chunk => {
-                body += chunk.toString();
-            });
-
-            request.on('end', () => {
-                // use parse() method
-                body = JSON.parse(body);
-
-                if (!('userName' in body || 'email' in body)){
-                    signIn.isSuccess = false;
-                    signIn.errorMessage = "atleast one of following is nedded: email or username";
-
-                    response.write( JSON.stringify(signIn) );
-                    response.end()
-                }
-                else if (!('password' in body)){
-                    signIn.isSuccess = false;
-                    signIn.errorMessage = "password is not given";
-
-                    response.write( JSON.stringify(signIn) );
-                    response.end()
-                }
-                else {
-                    checkSignInInfo(body.userName, body.email, body.password, signIn, response);
-                }
-            });
-        }
-    }
-    else {
-        response.end();
+        res.send(JSON.stringify(signUp));
     }
 });
 
-server.listen(80);
+
+// making API for signin
+
+app.get('/signIn', (req, res) => {
+    let signIn = {};
+
+    signIn.isSuccess = false;
+    signIn.errorMessage = `method can't be 'GET', it has to be 'POST'`;
+
+    res.send(JSON.stringify(signIn));
+});
 
 
-async function checkSignInInfo(userName, email, password, signIn, response){
-    
-    client.query(userName === undefined ? `SELECT * FROM person WHERE email='${email}'` : `SELECT * FROM person WHERE username='${userName}'`, async (err, result) => {
-        if (err){
-            console.log(err);
+app.post('/signIn', (req, res) => {
+    if (!('userName' in req.body || 'email' in req.body)){
+        let signIn = {};
+
+        signIn.isSuccess = false;
+        signIn.errorMessage = "atleast one of following is nedded: email or username";
+
+        res.send(JSON.stringify(signIn));
+    }
+    else if (!('password' in req.body)){
+        let signIn = {};
+
+        signIn.isSuccess = false;
+        signIn.errorMessage = "password is not given";
+
+        res.send(JSON.stringify(signIn))
+    } else {
+        checkSignInInfo(req.body.userName, req.body.email, req.body.password, res);
+    }
+});
+
+
+
+async function checkSignInInfo(userName, email, password, res){
+    try{
+        let result = await client.query(userName === undefined ? `SELECT * FROM person WHERE email=$1;` : `SELECT * FROM person WHERE username=$1;`, [userName === undefined ? email : userName]);
+
+        if (result.rowCount == 0){
+            let signIn = {};
+
             signIn.isSuccess = false;
-            signIn.errorMessage = "Some SQL error occured";
+            signIn.errorMessage = "username or email didn't match";
 
-            response.write( JSON.stringify(signIn) );
-            response.end();
-        }
-        else {
-            if (result.rowCount == 0){
-                signIn.isSuccess = false;
-                signIn.errorMessage = "username or email didn't match";
-
-                response.write( JSON.stringify(signIn) );
-                response.end();
-            }
-            else{
-
-                let doesPasswordMatch = await bcrypt.compare(password, result.rows[0].password);
-
-                if (doesPasswordMatch){
-                    signIn.isSuccess = true;
-                    signIn.userName = result.rows[0].username;
-                    signIn.firstName = result.rows[0].first_name;
-                    signIn.lastName = result.rows[0].last_name;
-                    signIn.email = result.rows[0].email;
-
-                    response.write( JSON.stringify(signIn) );
-                    response.end();
-                }
-                else{
-                    signIn.isSuccess = false;
-                    signIn.errorMessage = "password didn't match";
-
-                    response.write( JSON.stringify(signIn) );
-                    response.end();
-                }
-            }
-        }
-    });
-}
-
-async function addSignUpInfo(firstName, lastName, email, password, userName, signUp, response) {
-
-    client.query(`SELECT email,password FROM person WHERE email='${email}' or username='${userName}';`, async (err, result) => {
-        if (err){
-            console.log(err);
-            signUp.isSuccess = false;
-            signUp.errorMessage = "Some SQL error occured";
-
-            response.write( JSON.stringify(signUp) );
-            response.end();
+            res.send(JSON.stringify(signIn));
         } else {
-            if (result.rowCount != 0){
+            let doesPasswordMatch = await bcrypt.compare(password, result.rows[0].password);
 
-                signUp.isSuccess = false;
-                signUp.errorMessage = "username or email already exists";
+            if (!doesPasswordMatch){
+                let signIn = {};
 
-                response.write( JSON.stringify(signUp) );
-                response.end();
+                signIn.isSuccess = false;
+                signIn.errorMessage = `password didn't match`;
+
+                res.send(JSON.stringify(signIn));
             } else {
-                let passwordHash = await bcrypt.hash(password, 10);
+                let signIn = {};
 
-                client.query(`INSERT INTO person VALUES ('${userName}', '${firstName}', '${lastName}', '${email}', '${passwordHash}');`, (err, result) => {
-                    if (err){
-                        console.log(err);
-                        signUp.isSuccess = false;
-                        signUp.errorMessage = "Some SQL error occured";
+                signIn.isSuccess = true;
+                signIn.userName = result.rows[0].username;
+                signIn.firstName = result.rows[0].first_name;
+                signIn.lastName = result.rows[0].last_name;
+                signIn.email = result.rows[0].email;
 
-                        response.write( JSON.stringify(signUp) );
-                        response.end();
-                    } else {
-                        signUp.isSuccess = true;
-                        signUp.errorMessage = "";
-
-                        response.write( JSON.stringify(signUp) );
-                        response.end();
-                    }
-                });
+                res.send(JSON.stringify(signIn));
             }
         }
-    });
+
+    } catch(err) {
+        console.log(err);
+
+        let signIn = {};
+
+        signIn.isSuccess = false;
+        signIn.errorMessage = `some SQL error occured`;
+
+        res.send(JSON.stringify(signIn));
+    }
 
 }
+
+
+async function addSignUpInfo(firstName, lastName, email, password, userName, res){
+
+    // checking if username or password already exists
+    try{
+        const result = await client.query(`SELECT username FROM person WHERE username=$1 OR email=$2`, [userName, email]);
+
+        if (result.rowCount != 0){
+            let signUp = {};
+
+            signUp.isSuccess = false;
+            signUp.errorMessage = "username or email already exists";
+
+            res.send(JSON.stringify(signUp));
+        } else {
+            let passwordHash = await bcrypt.hash(password, 10);
+
+            const result = await client.query(`INSERT INTO person VALUES ($1, $2, $3, $4, $5)`, [userName, firstName, lastName, email, passwordHash]);
+            
+            let signUp = {};
+
+            signUp.isSuccess = true;
+            signUp.errorMessage = "";
+            
+            res.send(JSON.stringify(signUp));
+        }
+    } catch (err) {
+        console.log(err);
+
+        let signUp = {};
+
+        signUp.isSuccess = false;
+        signUp.errorMessage = `some SQL error occured`;
+
+        res.send(JSON.stringify(signUp));
+    }
+    
+
+}
+
+
+app.listen(80);
