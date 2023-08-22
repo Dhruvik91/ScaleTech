e28817a071dd47515cf1536b7f9dd717249d77e2
Divyanshu Motivaras <divyanshu1593@gmail.com>
2023-08-22 14:39:28 +0530

    adding API for signup with JSON as response

--- Backend/server_2.js
+++ Backend/server_2.js
@@ -1,65 +1,96 @@
 const express = require("express");
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+
+
+// setting environment variables
+require('dotenv').config();
+
+// connecting to database
+let client = new pg.Client(process.env.DB_URL);
+
+client.connect((err) => {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+});
+
+
 const app = express();
 
-//bodyParser : jyare aapde form ma enter kreli value retrive krvi hoy tyare bodyparser package vpray
-const bodyParser = require('body-parser');
+// middlewares
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-//express.json() is middleware, you enable your Express application to automatically parse incoming JSON data
-app.use(express.json());
 
-//password ne encrypt krva mate
-const bcrypt = require('bcryptjs');
-const saltRounds = 10;
+// making API for storing signup data to the database
 
-//database connection start
-const { Client } = require('pg');
+let signUp = {};
 
-const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'yourpassword',
-    database: 'myDB'
-})
-//database connetion end
+app.get('/signUp', (req, res) => {
 
-app.post('/register', async (req, res) => {
-    const { username, firstname, lastname, email, password } = req.body;
-    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+    signUp.isSuccess = false;
+    signUp.errorMessage = `method can't be 'GET', it has to be 'POST'`;
 
-    try {
-        const result = await client.query("insert into users(username,firstname,lastname,email,password) values($1,$2,$3,$4,$5)", [username, firstname, lastname, email, encryptedPassword]);
-        res.redirect('login');
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
+    res.send(JSON.stringify(signUp));
+});
+
+
+app.post('/signUp', (req, res) => {
+    if (('firstName' in req.body) && 
+    ('lastName' in req.body) && 
+    ('email' in req.body) && 
+    ('password' in req.body) && 
+    ('userName' in req.body)){
+
+        addSignUpInfo(req.body.firstName, req.body.lastName, req.body.email, req.body.password, req.body.userName, signUp, res);
+    } else {
+        // if some of the fields are missing in api request
+        signUp.isSuccess = false;
+        signUp.errorMessage = "Some of the fields were missing in post request";
+
+        res.send(JSON.stringify(signUp));
     }
 });
 
-app.post('/login', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    try {
-        const result = await client.query("SELECT * FROM users WHERE username = $1", [username]);
-        if (result.rows.length > 0) {
 
-            const isMatch = await bcrypt.compare(password, result.rows[0].password);
-            console.log(isMatch);
+async function addSignUpInfo(firstName, lastName, email, password, userName, signUp, res){
 
-            if (isMatch) {
-                res.redirect('/');
-            }
-            else {
-                res.redirect('/login');
-            }
+    // checking if username or password already exists
+    try{
+        const result = await client.query(`SELECT username FROM person WHERE username=$1 OR email=$2`, [userName, email]);
+
+        if (result.rowCount != 0){
+
+            signUp.isSuccess = false;
+            signUp.errorMessage = "username or email already exists";
+
+            response.send(JSON.stringify(signUp));
+        } else {
+            let passwordHash = await bcrypt.hash(password, 10);
+
+            const result = await client.query(`INSERT INTO person VALUES ($1, $2, $3, $4, $5)`, [userName, firstName, lastName, email, passwordHash]);
+            
+            signUp.isSuccess = true;
+            signUp.errorMessage = "";
+            
+            res.send(JSON.stringify(signUp));
         }
-    }
-    catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
+    } catch (err) {
+        console.log(err);
 
-});
+        signUp.isSuccess = false;
+        signUp.errorMessage = `some SQL error occured`;
 
+        res.send(JSON.stringify(signUp));
+    }
+    
+
+}
+
+
+app.listen(80);
