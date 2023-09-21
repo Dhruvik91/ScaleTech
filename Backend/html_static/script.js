20891f755feb9e3a649cb68f587a1dac77eeffdb
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-09-21 19:10:26 +0400

    just testing things

--- Backend/html_static/script.js
+++ Backend/html_static/script.js
@@ -1,75 +1,4 @@
-/* //localStorage.clear();
-
-// Function to create a blog
-function createBlog() {
-    const title = document.getElementById("Title").value;
-    const description = document.getElementById("Description").value;
-    const content = document.getElementById("Content").value;
-
-    // Check if local storage is available in the browser
-    if (typeof (Storage) !== "undefined") {
-        // Get existing blogs from local storage
-        const existingBlogs = JSON.parse(localStorage.getItem("blogs") || "[]");
-
-        // Create a new blog object
-        const newBlog = {
-            title: title,
-            description: description,
-            content: content,
-        };
-
-        // Add the new blog to the existing blogs
-        existingBlogs.push(newBlog);
-
-        // Save the updated blogs to local storage
-        localStorage.setItem("blogs", JSON.stringify(existingBlogs));
-
-        // Clear the form fields
-        document.getElementById("Title").value = "";
-        document.getElementById("Description").value = "";
-        document.getElementById("Content").value = "";
-
-        // Display the updated blog list
-        displayBlogs();
-    } else {
-        alert("Local storage is not supported in this browser.");
-    }
-}
-
-// Function to display the blogs
-function displayBlogs() {
-    const blogList = document.getElementById("blogList");
-
-    // Check if local storage is available in the browser
-    if (typeof (Storage) !== "undefined") {
-        // Get blogs from local storage
-        const blogs = JSON.parse(localStorage.getItem("blogs") || "[]");
-
-        // Clear the blog list
-        blogList.innerHTML = "";
-
-        // Loop through each blog and display it
-        blogs.forEach(blog => {
-            const blogDiv = document.createElement("div");
-            blogDiv.innerHTML = `
-                <h2>${blog.title}</h2>
-                <p>${blog.description}</p>
-                <p>${blog.content}</p>
-                <hr>
-            `;
-            blogList.appendChild(blogDiv);
-        });
-    } else {
-        alert("Local storage is not supported in this browser.");
-    }
-}
-
-// Display existing blogs on page load
-window.onload = function () {
-    displayBlogs();
-  }; */
-
-  //localStorage.clear();
+ // localStorage.clear();
 
 // Function to create a blog
 function createBlog() {
@@ -80,6 +9,7 @@ function createBlog() {
 
     // Check if local storage is available in the browser
     if (typeof (Storage) !== "undefined") {
+        
         // Get existing blogs from local storage
         const existingBlogs = JSON.parse(localStorage.getItem("blogs") || "[]");
 
@@ -88,7 +18,7 @@ function createBlog() {
             title: title,
             description: description,
             content: content,
-            tags: tags.split(",") // Assuming tags are comma-separated
+            tags: tags.split(",").map(tag => `<div class="tag">${tag}</div>`).join('') // Assuming tags are comma-separated
         };
 
         // Add the new blog to the existing blogs
